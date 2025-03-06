8d5f6fee638f7a0344b3ffe066d79b1025d72f70
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-07 02:14:57 +0600

    update table

--- src/components/modules/adminComponents/allmedicinpage/AllMedi.tsx
+++ src/components/modules/adminComponents/allmedicinpage/AllMedi.tsx
@@ -1,4 +1,6 @@
 import TitleMedicin from "@/components/shared/TitleMedicin";
+import ToolTipePage from "@/components/shared/ToolTipePage";
+import Image from "next/image";
 
 
 
@@ -18,14 +20,39 @@ const AllMedi = () => {
                             <thead className="ltr:text-left rtl:text-right">
                                 <tr>
                                     <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Name</th>
-                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Date of Birth</th>
-                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Role</th>
-                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Salary</th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Create Date</th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap ">
+                                        <ToolTipePage title="M. Name" tole="Manufacture Name" />
+                                    </th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap ">
+                                        <ToolTipePage title="M. Address" tole="Manufacture Address" />
+                                    </th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap ">
+                                        <ToolTipePage title="M. Contact" tole="Manufacture Contact" />
+                                    </th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap ">
+                                        <ToolTipePage title="Stock" tole="Stock Availability" />
+                                    </th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap ">
+                                        <ToolTipePage title="R. Prescription" tole="Required Prescription" />
+                                    </th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Mass Unit</th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Quantity</th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Price</th>
+                                    <th className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">Action</th>
                                 </tr>
                             </thead>
 
                             <tbody className="divide-y divide-gray-200">
                                 <tr>
+                                    <td>
+                                        <div className="flex items-center gap-x-2 p-1">
+                                            <Image height={20} width={20} className="object-cover w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+                                                <div>
+                                                    <h2 className="font-medium text-gray-800 dark:text-white ">Arthur Melo</h2>
+                                                </div>
+                                        </div>
+                                    </td>
                                     <td className="px-4 py-2 font-medium whitespace-nowrap text-gray-900">John Doe</td>
                                     <td className="px-4 py-2 whitespace-nowrap text-gray-700">24/05/1995</td>
                                     <td className="px-4 py-2 whitespace-nowrap text-gray-700">Web Developer</td>
---
+++ src/components/shared/ToolTipePage.tsx
@@ -0,0 +1,27 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+
+interface TTolls {
+    title: string;
+    tole: string;
+}
+
+const ToolTipePage = ({title, tole}: TTolls) => {
+
+    return (
+        <div>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        {/* <Button variant="outline">Hover</Button> */}
+                        <div className="px-4 py-2 font-medium whitespace-nowrap hover:text-blue-500">{title}</div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>{tole}</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+        </div>
+    );
+};
+
+export default ToolTipePage;
