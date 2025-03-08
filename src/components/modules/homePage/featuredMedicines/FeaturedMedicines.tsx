6deac239edeffef448f45ce6a9b0ac2f6df0016c
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-08 23:53:56 +0600

    data fatch success

--- src/components/modules/homePage/featuredMedicines/FeaturedMedicines.tsx
+++ src/components/modules/homePage/featuredMedicines/FeaturedMedicines.tsx
@@ -1,7 +1,42 @@
 
+"use client"
+
+import Loader from "@/components/shared/Loader";
+import { getAllMedicins } from "@/services/MedicinManagment";
+import { useEffect, useState } from "react";
+import MedicinCard from "./MedicinCard";
+import { TMedicine } from "@/types/medicins";
 
 
 const FeaturedMedicines = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [medicins, setMedicin] = useState([]);
+
+    const getMedicin = async (page: number) => {
+        setIsLoading(true);
+
+        try {
+            const { data: newMedicins } = await getAllMedicins(page, 6);
+            if (newMedicins) {
+                setMedicin(newMedicins?.result);
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+
+    useEffect(() => {
+        getMedicin(1);
+    }, []);
+
+
+    if (isLoading) {
+        return <Loader />;
+    }
 
     return (
         <div>
@@ -12,7 +47,13 @@ const FeaturedMedicines = () => {
                     </div>
                 </div>
             </div>
-            <div> The Component is Start FeaturedMedicines </div>
+
+            <div>
+                <div className='mx-auto mt-10 grid max-w-[1440px] grid-cols-1 gap-9 px-5 md:grid-cols-2 lg:grid-cols-3 lg:px-0'>
+                    {medicins?.map((medici: TMedicine) => <MedicinCard medici={medici} key={medici._id} />)}
+                </div>
+            </div>
+
         </div>
     );
 };
---
+++ src/components/modules/homePage/featuredMedicines/MedicinCard.tsx
@@ -0,0 +1,98 @@
+import { Button } from "@/components/ui/button";
+import { TMedicine } from "@/types/medicins";
+import Image from "next/image";
+
+interface TMedicinss {
+    medici: TMedicine
+}
+
+
+const MedicinCard = ({ medici }: TMedicinss) => {
+    console.log(medici);
+
+
+    return (
+        <div>
+            <div className='group'>
+                {/* Container for Image with relative positioning */}
+                <div className='relative h-60 w-full rounded-md bg-cover'>
+                    <div className='absolute bottom-5 left-7 z-10 mt-5 flex scale-y-0 items-center font-medium opacity-0 transition-all duration-500 ease-out group-hover:scale-y-100 group-hover:opacity-100'>
+                        <Button
+                            className='mr-2 w-fit rounded-[4px] border bg-white px-7 py-5 text-sm uppercase leading-4 text-black shadow-2xl duration-500 hover:border-[#DF2626]'
+                            // onClick={() => setModal2Open(true)}
+                        >
+                            Quick View
+                        </Button>
+                        {/* <Modal
+                            className='w-[1400px]'
+                            centered
+                            open={modal2Open}
+                            onCancel={() => setModal2Open(false)}
+                        >
+                            <div className='flex gap-8 '>
+                                <div className='relative flex-1'>
+                                    <Image
+                                        src={image}
+                                        alt={shoeName}
+                                        layout='fill'
+                                        objectFit='cover'
+                                        className='z-0 h-full w-full rounded-md'
+                                    />
+                                </div>
+                                <div className='flex-1'>
+                                    <h4 className='text-base font-semibold uppercase text-gray-500'>{category}</h4>
+                                    <h1 className='text-2xl font-semibold uppercase italic text-black'>{shoeName}</h1>
+                                    <p className='text-base font-semibold text-gray-500 mt-2'>{shortDescription}</p>
+                                    <p className='mb-4 mt-6 text-sm font-semibold text-gray-600 uppercase'>Price</p>
+                                    <h5 className='text-2xl font-medium uppercase text-black'>${price}</h5>
+                                    <p className='mb-4 mt-6 text-sm font-semibold text-gray-600 uppercase'>SIZE</p>
+                                    <div className='flex flex-wrap gap-2'>
+                                        {
+                                            sizes.map(s => (
+                                                <button key={s} onClick={() => setSize(s)} className={`py-2 hover:border-black :bg-black cursor-pointer px-4 border rounded-md border-gray-300 ${s === size ? 'bg-black text-white' : 'hover:border-black'}`}>{s}</button>
+                                            ))
+                                        }
+                                    </div>
+                                    <div className='mt-5 flex items-center font-medium transition-all duration-500 ease-out'>
+                                        <button onClick={() => addCart(shoe)} className='z-10 mr-2 rounded-[4px] border border-black px-6 py-3 leading-4 text-black shadow-2xl duration-500 hover:border-black hover:bg-black hover:text-white'>
+                                            ADD TO CARD
+                                        </button>
+                                        <button onClick={() => addToWishList(id)} className='z-10 rounded-[4px] bg-black px-6 py-[10px] text-white shadow-2xl duration-500 hover:bg-[#DF2626] hover:text-white hover:shadow-lg'>
+                                            Add To Wishlist
+                                        </button>
+                                        <button onClick={() => compareShoes(shoe)} className='z-10 rounded-[4px] bg-black px-6 py-[10px] ml-12 text-white shadow-2xl duration-500 hover:bg-[#DF2626] hover:text-white hover:shadow-lg'>
+                                            Compare
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </Modal> */}
+                        {/* <button onClick={() => addToWishList(id)} className='w-fit rounded-[4px] border bg-white px-3 py-3 text-sm uppercase leading-4 text-black shadow-2xl duration-500 hover:border-[#DF2626] active:bg-[#DF2626]'>
+                            <SiSouthwestairlines />
+                        </button> */}
+                    </div>
+
+                    <Image
+                        src={medici?.mediImage}
+                        alt={medici?.name}
+                        layout='fill'
+                        objectFit='cover'
+                        className='z-0 h-full w-full'
+                    />
+                </div>
+                <div className='mt-6'>
+                    <h3 className='z-50 w-fit cursor-pointer text-lg font-semibold duration-300 hover:text-[#DF2626]'>
+                        {medici?.name}
+                  
+                    </h3>
+                    <div className='mt-2 flex items-end gap-1'>
+                        <p className='text-xl font-bold text-[#DF2626]'>{medici?.price} TK</p>
+                        {/* <p className='text-base font-bold italic line-through'>{medici?.price} TK</p> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MedicinCard;
