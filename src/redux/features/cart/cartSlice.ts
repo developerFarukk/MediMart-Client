95d54beb586c4bb35bd7539e41f2d99e9901fe3c
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-11 03:18:47 +0600

    add card slice

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,14 @@
+
+import { createSlice } from "@reduxjs/toolkit";
+
+interface TInitialState {}
+
+const initialState: TInitialState = {};
+
+
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {}
+});
