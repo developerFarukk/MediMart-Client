3ea8f50a6d33eb7dde4960eb6c94b63ed493b1e1
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-12 01:27:23 +0600

    add overview card

---
+++ src/components/modules/customerPage/myCardPage/Cart.tsx
@@ -0,0 +1,60 @@
+import {  Minus, Plus, Trash } from "lucide-react";
+import Image from "next/image";
+
+
+// interface CartProps {
+//     idx: number;
+//     length: number;
+// }
+
+const Cart = (
+    // { idx, length }: CartProps
+) => {
+
+    return (
+        <div>
+            <div>
+                <div className='flex justify-between gap-4'>
+                    <div className='flex gap-5'>
+                        <div className='flex flex-col items-center gap-3 md:flex-row'>
+                            
+                            <div className='relative h-32 w-32 bg-cover'>
+                                <Image
+                                    className='w-full rounded-md border bg-[#EAEAEA] p-1'
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd5GoZzI6363LYhvWJrtfncBfsZ23U-rA6C9BDSyQRLXOe3Z3rij0vk9DhvpyG2k-nKWE&usqp=CAU"
+                                    alt='asdf'
+                                    layout='fill'
+                                    objectFit='cover'
+                                    // height={50}
+                                    // width={50}
+                                />
+                            </div>
+                        </div>
+                        <h3 className='items-start justify-start text-base font-bold text-[#434343]'>
+                            {/* {shoeName} */}
+                            Medi name
+                        </h3>
+                    </div>
+                    <div className='flex flex-col justify-between'>
+                        {/* <GoPlus
+                            onClick={() => deleteCart(id)}
+                            className='ml-auto rotate-45 cursor-pointer text-3xl text-[#939393]'
+                        /> */}
+                        <Trash
+                            // onClick={() => deleteCart(id)}
+                            className='ml-auto  cursor-pointer text-3xl'
+                        />
+                        <p className='text-xl font-semibold text-black'>
+                            {/* €{discountPrice} */}
+                            TK 1300
+                        </p>
+                    </div>
+                </div>
+
+                {/* {idx + 1 === length ? '' : <div className='divider my-5 border-none border-[#F4F4F4]'></div>} */}
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/modules/customerPage/myCardPage/MyCardPage.tsx
+++ src/components/modules/customerPage/myCardPage/MyCardPage.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Cart from "./Cart";
 
 
 
@@ -5,7 +7,60 @@ const MyCardPage = () => {
 
     return (
         <div>
-            <div> The Component is Start MyCardPage </div>
+            <div className='mb-24  border-t'>
+                <div className='mx-auto max-w-[1440px] pt-9'>
+                    <div className='lg:flow-row mx-auto flex w-[90%] flex-col gap-20 lg:w-[85%] lg:flex-row'>
+                        {/* cards */}
+                        <div className='flex-1'>
+                            <h3 className='text-2xl font-semibold'>An overview order</h3>
+
+                            {/* single cards */}
+                            <div className='gap mt-9 flex flex-col rounded-xl bg-gray-200 p-6'>
+                                {/* {cart?.map((shoe, idx) => <Cart key={shoe._id} idx={idx} length={cart.length} shoe={shoe}></Cart>)} */}
+                                <Cart  />
+                            </div>
+                        </div>
+
+                        {/* order details part */}
+                        <div>
+                            <h3 className='text-2xl font-semibold'>Oder details</h3>
+                            <div className=''>
+                                <div className='mb-4 mt-9 w-full rounded-xl border bg-[#FAFAFA] p-5 text-lg font-semibold *:w-full md:w-80'>
+                                    <div className='flex justify-between text-lg font-normal text-[#656565]'>
+                                        <h5>Subtotal</h5>
+                                        <p>
+                                            {/* € {grandTotal.toFixed(2)} */}
+                                            Total Price
+                                        </p>
+                                    </div>
+                                    <div className='mt-2 flex justify-between text-lg font-normal text-[#656565]'>
+                                        <h5>Shipping</h5>
+                                        <p>Free</p>
+                                    </div>
+                                    <div className='mt-2 flex justify-between border-b pb-4 text-lg font-normal text-[#656565]'>
+                                        <h5 className='flex items-center gap-2'>
+                                            Estimated Tax
+                                        </h5>
+                                        <p>€ 0</p>
+                                    </div>
+                                    <div>
+                                        <div className='mt-1 flex justify-between pt-4 text-xl font-semibold text-[#656565]'>
+                                            <h5 className='flex items-center gap-2 text-[#656565]'>Total</h5>
+                                            <p className='text-black'>
+                                                {/* € {grandTotal.toFixed(2)} */}
+                                                Grand Total
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <Button className="w-full">
+                                    <span className='-mb-1'>Go to Checkout</span>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
