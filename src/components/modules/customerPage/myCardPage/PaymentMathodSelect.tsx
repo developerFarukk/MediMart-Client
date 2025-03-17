378cde513389364737ce13bd55afa3dcca88e4b4
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-18 00:05:44 +0600

    add pay design

--- src/components/modules/customerPage/myCardPage/PaymentMathodSelect.tsx
+++ src/components/modules/customerPage/myCardPage/PaymentMathodSelect.tsx
@@ -10,20 +10,31 @@ const PaymentMathodSelect = () => {
 
     return (
         <div>
-            <RadioGroup defaultValue="comfortable">
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="default" id="r1" />
-                    <Label htmlFor="r1">Default</Label>
+
+            <div className=''>
+                <div className='mb-4 mt-9 w-full rounded-xl border bg-[#FAFAFA] p-5 text-lg font-semibold *:w-full md:w-80'>
+                    <div className="flex flex-col justify-between h-full">
+                        <h1 className="text-2xl font-bold">Slecte Payment Mathod</h1>
+                        <div className="mt-5">
+                            <RadioGroup defaultValue="comfortable">
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="default" id="r1" />
+                                    <Label htmlFor="r1">Default</Label>
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="comfortable" id="r2" />
+                                    <Label htmlFor="r2">Comfortable</Label>
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="compact" id="r3" />
+                                    <Label htmlFor="r3">Compact</Label>
+                                </div>
+                            </RadioGroup>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="comfortable" id="r2" />
-                    <Label htmlFor="r2">Comfortable</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="compact" id="r3" />
-                    <Label htmlFor="r3">Compact</Label>
-                </div>
-            </RadioGroup>
+            </div>
 
         </div>
     );
