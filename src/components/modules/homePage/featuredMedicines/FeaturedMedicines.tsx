32945900eee3a23e2341b9d811f71376bc8951c5
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-23 00:47:29 +0600

    update search

--- src/components/modules/homePage/featuredMedicines/FeaturedMedicines.tsx
+++ src/components/modules/homePage/featuredMedicines/FeaturedMedicines.tsx
@@ -1,20 +1,21 @@
 
-"use client"
+
+"use client";
 
 import Loader from "@/components/shared/Loader";
 import { getAllMedicins } from "@/services/MedicinManagment";
 import { useEffect, useState } from "react";
 import MedicinCard from "./MedicinCard";
 import { TMedicine } from "@/types/medicins";
-
+import { Input } from "@/components/ui/input";
 
 const FeaturedMedicines = () => {
-
     const [isDialogOpen, setIsDialogOpen] = useState(false);
-
     const [isLoading, setIsLoading] = useState(false);
-    const [medicins, setMedicin] = useState([]);
+    const [medicins, setMedicin] = useState<TMedicine[]>([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
+    // Fetch medicines
     const getMedicin = async (page: number) => {
         setIsLoading(true);
 
@@ -30,11 +31,16 @@ const FeaturedMedicines = () => {
         }
     };
 
+    // Filter medicines based on search query
+    const filteredMedicins = medicins.filter((medici) => {
+        const matchesName = medici.name.toLowerCase().includes(searchQuery.toLowerCase());
+        const matchesCategory = medici.category.toLowerCase().includes(searchQuery.toLowerCase());
+        return matchesName || matchesCategory;
+    });
 
     useEffect(() => {
         getMedicin(1);
     }, []);
-
 
     if (isLoading) {
         return <Loader />;
@@ -50,18 +56,29 @@ const FeaturedMedicines = () => {
                 </div>
             </div>
 
+            {/* Search Bar */}
+            <div className="flex justify-center mb-4">
+                <Input
+                    type="text"
+                    placeholder="Search by medicines name, category..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="lg:w-[40%] md:w-[60%] w-[95%]"
+                />
+            </div>
+
             <div>
                 <div className='mx-auto mt-10 grid max-w-[1440px] grid-cols-1 gap-9 px-5 md:grid-cols-2 lg:grid-cols-3 lg:px-0'>
-                    {medicins?.map((medici: TMedicine) =>
+                    {filteredMedicins?.map((medici: TMedicine) => (
                         <MedicinCard
                             medici={medici}
                             key={medici._id}
                             isDialogOpen={isDialogOpen}
                             setIsDialogOpen={setIsDialogOpen}
-                        />)}
+                        />
+                    ))}
                 </div>
             </div>
-
         </div>
     );
 };
