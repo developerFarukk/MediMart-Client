9508661aea8d382b030fdeba0358d1e92e68d8e4
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-23 00:04:25 +0600

    success all

--- src/app/(dashboardLayout)/customer/dashboard/page.tsx
+++ src/app/(dashboardLayout)/customer/dashboard/page.tsx
@@ -1,14 +1,30 @@
 
 
 import CustomerDashboardPage from "@/components/modules/adminComponents/CustomerDashboardPage";
+import Loader from "@/components/shared/Loader";
+import { getAllMyOrders } from "@/services/OrderMangment";
 
 
 
-const CustomerDashboard = () => {
+const CustomerDashboard = async () => {
+
+    const { data: orders, isLoading, isError } = await getAllMyOrders();
+
+    const myOrders = orders?.meta?.total
+
+    
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    if (isError) {
+        return <div>Data no fatch</div>;
+    }
 
     return (
         <div>
-            <CustomerDashboardPage />
+            <CustomerDashboardPage myOrders={myOrders} />
         </div>
     );
 };
--- src/components/modules/adminComponents/CustomerDashboardPage.tsx
+++ src/components/modules/adminComponents/CustomerDashboardPage.tsx
@@ -1,12 +1,29 @@
 
+interface TMyOrders {
+    myOrders: number
+}
 
 
-
-const CustomerDashboardPage = () => {
+const CustomerDashboardPage = ({myOrders}: TMyOrders) => {
 
     return (
         <div>
-            <div> The Component is Start CustomerDashboardPage </div>
+            <div className="flex flex-wrap justify-center gap-10 p-4 items-center mt-4">
+
+                {/* Total medicins board */}
+                <div className="w-60 mt-4 px-4 py-3 hover:bg-blue-100 bg-blue-50 border-blue-50 border-2 rounded-md shadow-md dark:bg-gray-800">
+                    <div>
+                        <h1 className="uppercase text-center font-bold">My Total Order </h1>
+                        {myOrders > 0 ?
+                            <h2 className="text-center text-4xl mb-5 font-bold mt-4">
+                                {myOrders}
+                            </h2> :
+                            <h2 className="text-center text-4xl mb-5 font-bold mt-4">0</h2>
+                        }
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
